59b56c8ac319282f49c25916420dae6fb3b7743e
Avinash <79407212+Avkr2021@users.noreply.github.com>
2021-06-02 13:36:02 +0530

    Create donate.php

---
+++ donate.php
@@ -0,0 +1,58 @@
+<?php
+  include 'trust.html';
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="homee.css">
+    <title>Donate</title>
+  </head>
+  <body>
+    <div class="donte">
+
+
+    <div class="donate_header">
+      <p>Donation</p>
+    </div>
+    <div class="donar">
+
+      <div class="AccountNo">
+
+        <div class="donr1">
+          <img class="hdfc" src="hdfc.png" alt="">
+        </div>
+
+        <div class="donr2">
+          <table>
+          <tr>
+            <td>Account No</td>
+            <td>5020  0055  5555  55</td>
+          </tr>
+          <tr>
+            <td>IFSC</td>
+            <td>HDFC00045812</td>
+          </tr>
+          <tr>
+            <td>Branch</td>
+            <td>Sangam vihar</td>
+          </tr>
+          <tr>
+            <td>Name</td>
+            <td>Blue Planet</td>
+          </tr>
+          </table>
+        </div>
+      </div>
+
+      <div class="accountQr">
+        <img src="bankqr.jpeg" alt="">
+      </div>
+
+    </div>
+
+    </div>
+  </body>
+</html>
