8344f8acedcb435fd2d9ebe0fcf451ba3968ff80
Avinash <79407212+Avkr2021@users.noreply.github.com>
2021-06-02 13:35:09 +0530

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,55 @@
+<?php
+  include 'trust.html';
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Contactt.css">
+    <title>Contact Us</title>
+  </head>
+  <body>
+
+    <div class="main-con">
+
+        <div class="head">
+          <h1>Address</h1>
+        </div>
+
+        <div class="Office">
+          <h2>Office Address</h2>
+          <p>Prop No - 684-A, Flat No - 3, UGF KH N9-684, Village Devli, New Delhi - 110080</p>
+
+          <table>
+            <tr>
+              <td>Mobile - </td>
+              <td>+91 2134567894</td>
+            </tr>
+            <tr>
+              <td>Email - </td>
+              <td>blueplanet@gmail.com</td>
+            </tr>
+          </table>
+        </div>
+
+        <div class="communicate">
+          <h2>Communication Address</h2>
+          <p> Sangam Vihar, New Delhi - 110062</p>
+
+          <table>
+            <tr>
+              <td>Mobile - </td>
+              <td>+91 2134567894</td>
+            </tr>
+            <tr>
+              <td>Email - </td>
+              <td>blueplanettrust9@gmail.com</td>
+            </tr>
+          </table>
+
+        </div>
+    </div>
+  </body>
+</html>
