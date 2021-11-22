f4fff380f636c1608a95c5db511406f04545a1d6
Avinash <79407212+Avkr2021@users.noreply.github.com>
2021-11-22 11:04:18 +0530

    Add files via upload

---
+++ TIACoin_two/earningteam.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <!-- css link -->
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <!-- bootstrap icon link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+    <!-- fafa icon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- font family -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400&display=swap" rel="stylesheet">
+    <title>TIS Coin</title>
+  </head>
+  <body>
+<!-- php include navbar -->
+<?php include('include/navbar.php'); 	?>
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<div class="earning_taem d-flex justify-content-between align-items-center mb-3">
+					<div class="earning_team_text">
+						<h4 class="mb-0">Earning Team</h4>
+					</div>
+					<div class="report_btn">
+						<a class="tuch_effect" href="#">Report</a>
+					</div>
+				</div>
+				<div class="earn_para">
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+				</div>		
+			</div>
+		</div>
+	</div>			
+</section>
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<div class="select_every text-right">
+					<select>
+						<option>Display everyone</option>
+						<option>Only active</option>
+						<option>Hide reported</option>
+					</select>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<div class="user_name">
+					<ul>
+						<li>
+							<div class="user_img">
+								<i class="bi bi-person"></i>
+							</div>
+							<div class="user_img_name">
+							<p><a href="#">Yash Tiwari</a></p>
+							<p>Mining</p>
+							</div>
+							<div class="user_img_two">
+								<span class="pie"><img src="assets/img/pie_user.png"></span>
+							</div>
+						</li>
+						<li>
+							<div class="user_img">
+								<i class="bi bi-person"></i>
+							</div>
+							<div class="user_img_name">
+							<p>Govind Singh</p>
+							<p>Inactive</p>
+							</div>
+						</li>
+						<li>
+							<div class="user_img">
+								<i class="bi bi-person"></i>
+							</div>
+							<div class="user_img_name">
+								<p><a href="#">Tushar Kushik</a></p>
+							  <p>Mining</p>
+							</div>
+							<div class="user_img_two">
+								<span class="pie"><img src="assets/img/pie_user.png"></span>
+							</div>
+						</li>
+						<li>
+							<div class="user_img">
+								<i class="bi bi-person"></i>
+							</div>
+							<div class="user_img_name">
+								<p><a href="#">Harsh Tiwari</a></p>
+							  <p>Inactive</p>
+							</div>
+						</li>
+						<li>
+							<div class="user_img">
+								<i class="bi bi-person"></i>
+							</div>
+							<div class="user_img_name">
+								<p><a href="#">Sumit ingh</a></p>
+							  <p>Inactive</p>
+							</div>
+						</li>
+						<li>
+							<div class="user_img">
+								<i class="bi bi-person"></i>
+							</div>
+							<div class="user_img_name">
+								<p><a href="#">Amit Rana</a></p>
+							  <p>Inactive</p>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<div class="ping_invite ping_invite_two">
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+					<div class="ping_invite_inner">
+						<a class="comman_yellow_btn" href="#">Invite</a> <a style="border: 1px solid #eee;" href="">Ping Inactive</a>
+					</div>
+					<!-- <div class="createte">
+						<a href="">Create Earning Team Chat</a>
+					</div> -->
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include('include/footer.php'); ?>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/script.js"></script>
+  </body>
+</html>
